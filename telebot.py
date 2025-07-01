970bbc4a63ab4280f6b42f3801a756fe5acf498c
1juanobrero <1juanobrero@gmail.com>
2025-07-01 12:25:46 -0500

    Add files via upload

---
+++ telebot.py
@@ -0,0 +1,12 @@
+import telebot
+
+# Reemplaza con tu token de bot
+BOT_TOKEN = '6973429821:AAGmCkWXIvHMN7yy_WbXcMNDoYP_zVHGaNk'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()

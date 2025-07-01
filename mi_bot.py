f683b81c62524f1dc9387bf2736f8ddd4b2dba94
1juanobrero <1juanobrero@gmail.com>
2025-07-01 15:23:55 -0500

    Update mi_bot.py
    si

--- mi_bot.py
+++ mi_bot.py
@@ -1,11 +1,19 @@
 import telebot
 
-BOT_TOKEN = '6973429821:AAGmCkWXIvHMN7yy_WbXcMNDoYP_zVHGaNk'  # Reemplaza con tu token
+BOT_TOKEN = '6973429821:AAGmCkWXIvHMN7yy_WbXcMNDoYP_zVHGaNk'
 
 bot = telebot.Bot(token=BOT_TOKEN)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hola! Bienvenido a mi bot.  Usa /chiste para un chiste.")
+
+@bot.message_handler(commands=['chiste'])
+def send_joke(message):
+    chistes = [
+        "¿Por qué los programadores prefieren el color negro? Porque tienen más #.",
+        "Un hombre entra a un bar con un cubo de cemento...El camarero le dice: ¿Pide algo?, El hombre responde: Nop. Solo estoy esperando que se seque el cemento."
+    ]
+    bot.reply_to(message, chistes[0]) # Para empezar con el primer chiste.
 
 bot.infinity_polling()
